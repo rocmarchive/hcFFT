34c8d8bee489aa504736809c250beac4701ef8be
Meena <meena@multicorewareinc.com>
2015-06-23 14:48:48 +0530

    Add function to determine work group size and number of transforms per work group

--- source/stockham.cpp
+++ source/stockham.cpp
@@ -177,6 +177,121 @@ namespace StockhamGenerator
 			}
 		}
 	};
+
+	// Given the length of 1d fft, this function determines the appropriate work group size
+	// and the number of transforms per work group
+	// TODO for optimizations - experiment with different possibilities for work group sizes and num transforms for improving performance
+	void DetermineSizes(const size_t &MAX_WGS, const size_t &length, size_t &workGroupSize, size_t &numTrans)
+	{
+		assert(MAX_WGS >= 64);
+
+		if(length == 1) // special case
+		{
+			workGroupSize = 64;
+			numTrans = 64;
+			return;
+		}
+
+		size_t baseRadix[] = {5,3,2}; // list only supported primes
+		size_t baseRadixSize = sizeof(baseRadix)/sizeof(baseRadix[0]);
+
+		size_t l = length;
+		std::map<size_t, size_t> primeFactors;
+		std::map<size_t, size_t> primeFactorsExpanded;
+		for(size_t r=0; r<baseRadixSize; r++)
+		{
+			size_t rad = baseRadix[r];
+			size_t p = 0;
+			size_t e = 1;
+			while(!(l%rad))
+			{
+				l /= rad;
+				e *= rad;
+				p++;
+			}
+
+			primeFactors[rad] = p;
+			primeFactorsExpanded[rad] = e;
+		}
+
+		assert(l == 1); // Makes sure the number is composed of only supported primes
+
+		if (primeFactorsExpanded[2] == length)	// Length is pure power of 2
+		{
+			//if(length == 1024) { workGroupSize = 128;  numTrans = 1; }
+			if		(length >= 1024)	{ workGroupSize = (MAX_WGS >= 256) ? 256 : MAX_WGS; numTrans = 1; }
+			//else if (length == 512)		{ workGroupSize = (MAX_WGS >= 128) ? 128 : MAX_WGS; numTrans = 1; }
+			else if (length == 512)		{ workGroupSize = 64; numTrans = 1; }
+			else if	(length >= 16)		{ workGroupSize = 64;  numTrans = 256/length; }
+			else						{ workGroupSize = 64;  numTrans = 128/length; }
+		}
+		else if	(primeFactorsExpanded[3] == length) // Length is pure power of 3
+		{
+			workGroupSize = (MAX_WGS >= 256) ? 243 : 27;
+			if(length >= 3*workGroupSize)	numTrans = 1;
+			else							numTrans = (3*workGroupSize)/length;
+		}
+		else if	(primeFactorsExpanded[5] == length) // Length is pure power of 5
+		{
+			workGroupSize = (MAX_WGS >= 128) ? 125 : 25;
+			if(length >= 5*workGroupSize)	numTrans = 1;
+			else							numTrans = (5*workGroupSize)/length;
+		}
+		else
+		{
+			size_t leastNumPerWI; // least number of elements in one work item
+			size_t maxWorkGroupSize; // maximum work group size desired
+
+			if		(primeFactorsExpanded[2] * primeFactorsExpanded[3] == length) // Length is mix of 2&3 only
+			{
+				if(!(length%12))	{ leastNumPerWI = 12; maxWorkGroupSize = (MAX_WGS >= 128) ? 128 : MAX_WGS; }
+				else				{ leastNumPerWI = 6;  maxWorkGroupSize = (MAX_WGS >= 256) ? 256 : MAX_WGS; }
+			}
+			else if	(primeFactorsExpanded[2] * primeFactorsExpanded[5] == length) // Length is mix of 2&5 only
+			{
+				if(!(length%20))	{ leastNumPerWI = 20; maxWorkGroupSize = 64; }
+				else				{ leastNumPerWI = 10; maxWorkGroupSize = (MAX_WGS >= 128) ? 128 : MAX_WGS; }
+			}
+			else if (primeFactorsExpanded[3] * primeFactorsExpanded[5] == length) // Length is mix of 3&5 only
+			{
+				leastNumPerWI = 15;
+				maxWorkGroupSize = 64;
+			}
+			else
+			{
+				leastNumPerWI = 30;
+				maxWorkGroupSize = 64;
+			}
+
+
+			// Make sure the work group size does not exceed MAX_WGS
+			// for large problems sizes, this means doing more work per work-item
+			size_t lnpi;
+			size_t ft = 1;
+			while(1)
+			{
+				lnpi = leastNumPerWI * ft++;
+				if(length%lnpi) continue;
+
+				if( (length/lnpi) <= MAX_WGS )
+				{
+					leastNumPerWI = lnpi;
+					break;
+				}
+			}
+
+			numTrans = 1;
+			size_t n=1;
+			while( ((n*length)/leastNumPerWI) <= maxWorkGroupSize )
+			{
+				numTrans = n;
+				n++;
+			}
+
+			workGroupSize = (numTrans*length)/leastNumPerWI;
+			assert(workGroupSize <= MAX_WGS);
+		}
+	}
 }
 
 template<>
