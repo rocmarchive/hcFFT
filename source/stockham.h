f9cea5c72032049eeb0fa07af653de4ee7c75563
Meena <meena@multicorewareinc.com>
2015-06-23 14:30:06 +0530

    Add function to find Register base type

--- source/stockham.h
+++ source/stockham.h
@@ -43,4 +43,33 @@ namespace StockhamGenerator
 		ss << std::scientific << f;
 		return ss.str();
 	}
+
+	// Register data base types
+	template <Precision PR>
+	inline std::string RegBaseType(size_t count)
+	{
+		switch(PR)
+		{
+		case P_SINGLE:
+			switch(count)
+			{
+			case 1: return "float";
+			case 2: return "float_2";
+			case 4: return "float_4";
+			default: assert(false); return "";
+			}
+			break;
+		case P_DOUBLE:
+			switch(count)
+			{
+			case 1: return "double";
+			case 2: return "double_2";
+			case 4: return "double_4";
+			default: assert(false); return "";
+			}
+			break;
+		default:
+			assert(false); return "";
+		}
+	}
 }
