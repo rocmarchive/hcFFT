2dc04e6b7f7cb25bae49d9ff42a5ef245eae5f3d
Meena <meena@multicorewareinc.com>
2015-06-23 14:28:42 +0530

    Add integerToStr and FloatToStr functions

--- source/stockham.h
+++ source/stockham.h
@@ -16,4 +16,31 @@ namespace StockhamGenerator
 		P_SINGLE,
 		P_DOUBLE,
 	};
+
+	template <Precision PR>
+	inline size_t PrecisionWidth()
+	{
+		switch(PR)
+		{
+		case P_SINGLE:	return 1;
+		case P_DOUBLE:	return 2;
+		default:		assert(false); return 1;
+		}
+	}
+
+	// Convert unsigned integers to string
+	inline std::string SztToStr(size_t i)
+	{
+		std::stringstream ss;
+		ss << i;
+		return ss.str();
+	}
+
+	inline std::string FloatToStr(double f)
+	{
+		std::stringstream ss;
+		ss.precision(16);
+		ss << std::scientific << f;
+		return ss.str();
+	}
 }
