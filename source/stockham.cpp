326bd2d6a698c7956213e112bd0aaed0dbe8260c
Meena <meena@multicorewareinc.com>
2015-06-23 14:45:17 +0530

    Add Documentation and Empty stockhamgenerator namespace

--- source/stockham.cpp
+++ source/stockham.cpp
@@ -1,4 +1,85 @@
+#include <math.h>
 #include "stockham.h"
+#include <list>
+// FFT Stockham Autosort Method
+//
+//   Each pass does one digit reverse in essence. Hence by the time all passes are done, complete
+//   digit reversal is done and output FFT is in correct order. Intermediate FFTs are stored in natural order,
+//   which is not the case with basic Cooley-Tukey algorithm. Natural order in intermediate data makes it
+//   convenient for stitching together passes with different radices.
+//
+//  Basic FFT algorithm:
+//
+//        Pass loop
+//        {
+//            Outer loop
+//            {
+//                Inner loop
+//                {
+//                }
+//            }
+//        }
+//
+//  The sweeps of the outer and inner loop resemble matrix indexing, this matrix changes shape with every pass as noted below
+//
+//   FFT pass diagram (radix 2)
+//
+//                k            k+R                                    k
+//            * * * * * * * * * * * * * * * *                     * * * * * * * *
+//            *   |             |           *                     *   |         *
+//            *   |             |           *                     *   |         *
+//            *   |             |           * LS        -->       *   |         *
+//            *   |             |           *                     *   |         *
+//            *   |             |           *                     *   |         *
+//            * * * * * * * * * * * * * * * *                     *   |         *
+//                         RS                                     *   |         * L
+//                                                                *   |         *
+//                                                                *   |         *
+//                                                                *   |         *
+//                                                                *   |         *
+//                                                                *   |         *
+//                                                                *   |         *
+//                                                                *   |         *
+//                                                                * * * * * * * *
+//                                                                       R
+//
+//
+//    With every pass, the matrix doubles in height and halves in length
+//
+//
+//  N = 2^T = Length of FFT
+//  q = pass loop index
+//  k = outer loop index = (0 ... R-1)
+//  j = inner loop index = (0 ... LS-1)
+//
+//  Tables shows how values change as we go through the passes
+//
+//    q | LS   |  R   |  L  | RS
+//   ___|______|______|_____|___
+//    0 |  1   | N/2  |  2  | N
+//    1 |  2   | N/4  |  4  | N/2
+//    2 |  4   | N/8  |  8  | N/4
+//    . |  .   | .    |  .  | .
+//  T-1 |  N/2 | 1    |  N  | 2
+//
+//
+//   Data Read Order
+//     Radix 2: k*LS + j, (k+R)*LS + j
+//     Radix 3: k*LS + j, (k+R)*LS + j, (k+2R)*LS + j
+//     Radix 4: k*LS + j, (k+R)*LS + j, (k+2R)*LS + j, (k+3R)*LS + j
+//     Radix 5: k*LS + j, (k+R)*LS + j, (k+2R)*LS + j, (k+3R)*LS + j, (k+4R)*LS + j
+//
+//   Data Write Order
+//       Radix 2: k*L + j, k*L + j + LS
+//       Radix 3: k*L + j, k*L + j + LS, k*L + j + 2*LS
+//       Radix 4: k*L + j, k*L + j + LS, k*L + j + 2*LS, k*L + j + 3*LS
+//       Radix 5: k*L + j, k*L + j + LS, k*L + j + 2*LS, k*L + j + 3*LS, k*L + j + 4*LS
+//
+
+namespace StockhamGenerator
+{
+
+}
 
 template<>
 ampfftStatus FFTPlan::GetMax1DLengthPvt<Stockham> (size_t * longest) const
