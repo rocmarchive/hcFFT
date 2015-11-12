b5c815d5bdf9425050ff6e31b43abf4deee75676
Meena <meena@multicorewareinc.com>
2015-11-12 19:23:08 +0530

    Add routine to determine work group sizes for transpose kernel

--- source/transpose.cpp
+++ source/transpose.cpp
@@ -3,6 +3,23 @@
 
 #include "stockham.h"
 
+// A structure that represents a bounding box or tile, with convenient names for the row and column addresses
+// local work sizes
+struct tile
+{
+    union
+    {
+        size_t x;
+        size_t col;
+    };
+
+    union
+    {
+        size_t y;
+        size_t row;
+    };
+};
+
 template<>
 hcfftStatus FFTPlan::GetKernelGenKeyPvt<Transpose> (FFTKernelGenKeyParams & params) const
 {
@@ -65,6 +82,52 @@ hcfftStatus FFTPlan::GetKernelGenKeyPvt<Transpose> (FFTKernelGenKeyParams & para
     // CL_DEVICE_MAX_WORK_ITEM_SIZES
     params.fft_R = 1; // Dont think i'll use
     params.fft_SIMD = pEnvelope->limit_WorkGroupSize; // Use devices maximum workgroup size
+
+    return HCFFT_SUCCESS;
+}
+
+// Constants that specify the bounding sizes of the block that each workgroup will transpose
+const tile lwSize = { {16}, {16} };
+const size_t reShapeFactor = 4;   // wgTileSize = { lwSize.x * reShapeFactor, lwSize.y / reShapeFactor }
+const size_t outRowPadding = 0;
+
+// This is global, but should consider to be part of FFTPlan
+size_t loopCount = 0;
+tile blockSize = {{0}, {0}};
+
+template<>
+hcfftStatus FFTPlan::GetWorkSizesPvt<Transpose> (std::vector<size_t> & globalWS, std::vector<size_t> & localWS) const
+{
+    FFTKernelGenKeyParams fftParams;
+    this->GetKernelGenKeyPvt<Transpose>( fftParams );
+
+    // We need to make sure that the global work size is evenly divisible by the local work size
+    // Our transpose works in tiles, so divide tiles in each dimension to get count of blocks, rounding up for remainder items
+
+    size_t numBlocksX = fftParams.transOutHorizontal ?
+			DivRoundingUp(fftParams.fft_N[ 1 ], blockSize.y ) :
+			DivRoundingUp(fftParams.fft_N[ 0 ], blockSize.x );
+    size_t numBlocksY = fftParams.transOutHorizontal ?
+			DivRoundingUp( fftParams.fft_N[ 0 ], blockSize.x ) :
+			DivRoundingUp( fftParams.fft_N[ 1 ], blockSize.y );
+    size_t numWIX = numBlocksX * lwSize.x;
+
+    // Batches of matrices are lined up along the Y axis, 1 after the other
+    size_t numWIY = numBlocksY * lwSize.y * this->batchSize;
+    // fft_DataDim has one more dimension than the actual fft data, which is devoted to batch.
+    // dim from 2 to fft_DataDim - 2 are lined up along the Y axis
+    for(int i = 2; i < fftParams.fft_DataDim - 1; i++)
+    {
+	numWIY *= fftParams.fft_N[i];
+    }
+
+    globalWS.clear( );
+    globalWS.push_back( numWIX );
+    globalWS.push_back( numWIY );
+
+    localWS.clear( );
+    localWS.push_back( lwSize.x );
+    localWS.push_back( lwSize.y );
 
     return HCFFT_SUCCESS;
 }
