91fd55b18ad8d17119697d8adedc629b64f95da4
Neelakandan <neelakandan@multicorewareinc.com>
2015-12-26 06:16:12 -0600

    [Add unit test] Test create 2d and 3d plans

--- test/unit/hcfft_Create_Destroy_Plan/hcfft_Create_destroy_Plan.cpp
+++ test/unit/hcfft_Create_Destroy_Plan/hcfft_Create_destroy_Plan.cpp
@@ -1,9 +1,75 @@
 #include "hcfft.h"
 #include "gtest/gtest.h"
 
-TEST(hcfft_Create_Destroy_Plan, create_destroy_1D_plan ) {
+#define VECTOR_SIZE 1024
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_1D_plan_R2C ) {
   hcfftHandle *plan;
-  hcfftResult status  = hcfftPlan1d(plan, 1024, HCFFT_R2C);
+  hcfftResult status  = hcfftPlan1d(plan, VECTOR_SIZE, HCFFT_R2C);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_2D_plan_R2C ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan2d(plan, VECTOR_SIZE, VECTOR_SIZE,  HCFFT_R2C);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_3D_plan_R2C ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan3d(plan, VECTOR_SIZE, VECTOR_SIZE, VECTOR_SIZE, HCFFT_R2C);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_1D_plan_C2R ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan1d(plan, VECTOR_SIZE, HCFFT_C2R);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_2D_plan_C2R ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan2d(plan, VECTOR_SIZE, VECTOR_SIZE,  HCFFT_C2R);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_3D_plan_C2R ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan3d(plan, VECTOR_SIZE, VECTOR_SIZE, VECTOR_SIZE, HCFFT_C2R);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_1D_plan_C2C ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan1d(plan, VECTOR_SIZE, HCFFT_C2C);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_2D_plan_C2C ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan2d(plan, VECTOR_SIZE, VECTOR_SIZE,  HCFFT_C2C);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  //status =  hcfftDestroy(*plan);
+  //EXPECT_EQ(status, HCFFT_SUCCESS);
+}
+
+TEST(hcfft_Create_Destroy_Plan, create_destroy_3D_plan_C2C ) {
+  hcfftHandle *plan;
+  hcfftResult status  = hcfftPlan3d(plan, VECTOR_SIZE, VECTOR_SIZE, VECTOR_SIZE, HCFFT_C2C);
   EXPECT_EQ(status, HCFFT_SUCCESS);
   //status =  hcfftDestroy(*plan);
   //EXPECT_EQ(status, HCFFT_SUCCESS);
