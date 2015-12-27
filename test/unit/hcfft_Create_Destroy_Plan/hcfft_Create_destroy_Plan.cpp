bbea989c4059f44aae10bd7f5c6268aab403eb30
Meena <meena@multicorewareinc.com>
2015-12-27 13:08:41 -0600

    Add test to execute 2D R2C Transform

--- test/unit/hcfft_Create_Destroy_Plan/hcfft_Create_destroy_Plan.cpp
+++ test/unit/hcfft_Create_Destroy_Plan/hcfft_Create_destroy_Plan.cpp
@@ -1,7 +1,7 @@
 #include "hcfft.h"
 #include "gtest/gtest.h"
 
-#define VECTOR_SIZE 1024
+#define VECTOR_SIZE 256
 
 TEST(hcfft_Create_Destroy_Plan, create_destroy_1D_plan_R2C ) {
   hcfftHandle *plan = NULL;
@@ -14,6 +14,14 @@ TEST(hcfft_Create_Destroy_Plan, create_destroy_1D_plan_R2C ) {
 TEST(hcfft_Create_Destroy_Plan, create_destroy_2D_plan_R2C ) {
   hcfftHandle *plan = NULL;
   hcfftResult status  = hcfftPlan2d(plan, VECTOR_SIZE, VECTOR_SIZE,  HCFFT_R2C);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  int Rsize = VECTOR_SIZE * VECTOR_SIZE;
+  int Csize = VECTOR_SIZE * (1 + VECTOR_SIZE / 2);
+  hcfftReal *input = (hcfftReal*)calloc(Rsize, sizeof(hcfftReal));
+  hcfftComplex *output = (hcfftComplex*)calloc(Csize, sizeof(hcfftComplex));
+  Concurrency::array_view<hcfftReal, 1> idata(Rsize, input);
+  Concurrency::array_view<hcfftComplex, 1> odata(Csize, output);
+  status = hcfftExecR2C(*plan, &idata, &odata);
   EXPECT_EQ(status, HCFFT_SUCCESS);
   status =  hcfftDestroy(*plan);
   EXPECT_EQ(status, HCFFT_SUCCESS);
