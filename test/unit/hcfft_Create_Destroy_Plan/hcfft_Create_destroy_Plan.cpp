089391daf374dc9d560e76f4facecbb1837814ae
Neelakandan <neelakandan@multicorewareinc.com>
2015-12-27 22:16:55 -0600

    [Add unit test] hcfft_2D_transform test

--- test/unit/hcfft_Create_Destroy_Plan/hcfft_Create_destroy_Plan.cpp
+++ test/unit/hcfft_Create_Destroy_Plan/hcfft_Create_destroy_Plan.cpp
@@ -15,14 +15,6 @@ TEST(hcfft_Create_Destroy_Plan, create_destroy_2D_plan_R2C ) {
   hcfftHandle *plan = NULL;
   hcfftResult status  = hcfftPlan2d(plan, VECTOR_SIZE, VECTOR_SIZE,  HCFFT_R2C);
   EXPECT_EQ(status, HCFFT_SUCCESS);
-  int Rsize = VECTOR_SIZE * VECTOR_SIZE;
-  int Csize = VECTOR_SIZE * (1 + VECTOR_SIZE / 2);
-  hcfftReal *input = (hcfftReal*)calloc(Rsize, sizeof(hcfftReal));
-  hcfftComplex *output = (hcfftComplex*)calloc(Csize, sizeof(hcfftComplex));
-  Concurrency::array_view<hcfftReal, 1> idata(Rsize, input);
-  Concurrency::array_view<hcfftComplex, 1> odata(Csize, output);
-  status = hcfftExecR2C(*plan, &idata, &odata);
-  EXPECT_EQ(status, HCFFT_SUCCESS);
   status =  hcfftDestroy(*plan);
   EXPECT_EQ(status, HCFFT_SUCCESS);
 }
