5a79d3a9ee3602a6abe62b02b125627ec987cf4a
Meena <meena@multicorewareinc.com>
2015-12-28 21:02:53 +0530

    [Add unit test] hcfft 2D transform for C2R

--- test/unit/hcfft_2D_transform/hcfft_2D_transform.cpp
+++ test/unit/hcfft_2D_transform/hcfft_2D_transform.cpp
@@ -18,3 +18,19 @@ TEST(hcfft_2D_transform_test, func_correct_2D_transform_R2C ) {
   status =  hcfftDestroy(*plan);
   EXPECT_EQ(status, HCFFT_SUCCESS);
 }
+
+TEST(hcfft_2D_transform_test, func_correct_2D_transform_C2R ) {
+  hcfftHandle *plan = NULL;
+  hcfftResult status  = hcfftPlan2d(plan, VECTOR_SIZE, VECTOR_SIZE,  HCFFT_C2R);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  int Csize = VECTOR_SIZE * (1 + VECTOR_SIZE / 2);
+  int Rsize = VECTOR_SIZE * VECTOR_SIZE;
+  hcfftComplex *input = (hcfftComplex*)calloc(Csize, sizeof(hcfftComplex));
+  hcfftReal *output = (hcfftReal*)calloc(Rsize, sizeof(hcfftReal));
+  Concurrency::array_view<hcfftComplex, 1> idata(Csize, input);
+  Concurrency::array_view<hcfftReal, 1> odata(Rsize, output);
+  status = hcfftExecC2R(*plan, &idata, &odata);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+  status =  hcfftDestroy(*plan);
+  EXPECT_EQ(status, HCFFT_SUCCESS);
+}
