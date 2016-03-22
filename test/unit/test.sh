27e73132dd0604466137dfe7e08c73891761f7b3
Meena <meena@multicorewareinc.com>
2016-03-22 16:04:56 +0530

    Update test script

--- test/unit/test.sh
+++ test/unit/test.sh
@@ -10,11 +10,11 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 # Move to gtest bin
-working_dir1="$current_work_dir/../../build/test/unit/gtest/bin/"
+working_dir1="$current_work_dir/../../build/test/unit/gtest/hcfft_Create_Destroy_Plan/bin/"
 cd $working_dir1
 
 #Gtest functions
-unittest="$working_dir1/unittest"
+unittest="$working_dir1/hcfft_Create_destroy_Plan"
 
 runcmd1="$unittest >> gtestlog.txt"
 eval $runcmd1
@@ -22,8 +22,62 @@ eval $runcmd1
 Log_file="$working_dir1/gtestlog.txt"
 if grep -q FAILED "$Log_file";
 then
-    echo "${red}GTEST               ----- [ FAILED ]${reset}"
+    echo "${red}hcfft_Create_destroy_Plan               ----- [ FAILED ]${reset}"
 else
-    echo "${green}GTEST             ----- [ PASSED ]${reset}"
+    echo "${green}hcfft_Create_destroy_Plan             ----- [ PASSED ]${reset}"
     rm $working_dir1/gtestlog.txt
 fi
+
+testdirectories=(hcfft_1D_transform hcfft_2D_transform hcfft_3D_transform)
+
+## now loop through the above array
+for i in 0 1 2
+do
+  working_dir1="$current_work_dir/../../build/test/unit/gtest/${testdirectories[$i]}/bin/"
+  cd $working_dir1
+
+  #Gtest functions
+  unittest="$working_dir1/${testdirectories[$i]}"
+
+  runcmd1="$unittest >> gtestlog.txt"
+  eval $runcmd1
+
+  Log_file="$working_dir1/gtestlog.txt"
+  if grep -q FAILED "$Log_file";
+  then
+      echo "${red}${testdirectories[$i]}               ----- [ FAILED ]${reset}"
+  else
+      echo "${green}${testdirectories[$i]}             ----- [ PASSED ]${reset}"
+      rm $working_dir1/gtestlog.txt
+  fi
+
+  #Gtest functions
+  unittest="$working_dir1/${testdirectories[$i]}_double"
+
+  runcmd1="$unittest >> gtestlog.txt"
+  eval $runcmd1
+
+  Log_file="$working_dir1/gtestlog.txt"
+  if grep -q FAILED "$Log_file";
+  then
+      echo "${red}${testdirectories[$i]}_double               ----- [ FAILED ]${reset}"
+  else
+      echo "${green}${testdirectories[$i]}_double           ----- [ PASSED ]${reset}"
+      rm $working_dir1/gtestlog.txt
+  fi
+
+  #Gtest functions
+  unittest="$working_dir1/${testdirectories[$i]}_padding"
+
+  runcmd1="$unittest >> gtestlog.txt"
+  eval $runcmd1
+
+  Log_file="$working_dir1/gtestlog.txt"
+  if grep -q FAILED "$Log_file";
+  then
+      echo "${red}${testdirectories[$i]}_padding               ----- [ FAILED ]${reset}"
+  else
+      echo "${green}${testdirectories[$i]}_padding           ----- [ PASSED ]${reset}"
+      rm $working_dir1/gtestlog.txt
+  fi
+done
