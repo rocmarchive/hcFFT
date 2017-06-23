3c21b4c94c46b23fd957967e65a23477ac83c103
Santhosh Kumar Rajamanickam <santhosh@multicorewareinc.com>
2017-06-23 14:33:53 +0000

    Fix symlink creation bug
    - Fixed symlink bug. Symlink libhipfft.so will be created under
      /opt/rocm/hcfft/lib and /opt/rocm/lib when --install is enabled.
    - Add support for --deepclean which cleans files under /opt/rocm/hcfft
      when enables during running ./clean.sh
    
    hcfft: symlink-bug

--- build.sh
+++ build.sh
@@ -128,6 +128,7 @@ set +e
 # MAKE BUILD DIR
 mkdir -p $current_work_dir/build
 mkdir -p $current_work_dir/build/lib
+mkdir $current_work_dir/build/packaging
 set -e
 
 # SET BUILD DIR
--- clean.sh
+++ clean.sh
@@ -4,8 +4,50 @@
 # CURRENT_WORK_DIRECTORY
 current_work_dir=$PWD
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+install=0
+
+# Help menu
+print_help() {
+cat <<-HELP
+=============================================================================================================================
+Please provide the following arguments:
+
+  ${green}--deepclean${reset}  To clean the files under /opt/rocm/hcfft  Requires sudo perms.
+
+=============================================================================================================================
+Usage: ./clean.sh --deepclean 
+=============================================================================================================================
+HELP
+exit 0
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --deepclean)
+      deepclean="1"
+      ;;
+    --help) print_help;;
+    *)
+      printf "************************************************************\n"
+      printf "* Error: Invalid arguments, run --help for valid arguments.*\n"
+      printf "************************************************************\n"
+      exit 1
+  esac
+  shift
+done
+
 # Remove build
 rm -rf $current_work_dir/build
+
+# Remove files under /opt/rocm/hcrng
+if [ "$deepclean" = "1" ]; then
+  sudo rm -rf /opt/rocm/hcfft
+  sudo rm -f /opt/rocm/lib/libhcfft.so
+  sudo rm -f /opt/rocm/lib/libhipfft.so
+fi
 
 # Remove temporarly cached kernel shared objects and binaries
 rm -rf $HOME/kernCache
